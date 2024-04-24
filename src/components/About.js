c330371f4758bec21a12f932730cfbe18d27232e
dan20 <kiptoodaniel92@gmail.com>
2024-04-24 21:47:17 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { image } from "../data/data";
+import image from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return <div id="About">;
+    <h2>About me</h2>
+    <p>I Made This</p>
+    <img src={ "https://i.imgur.com/mV8PQxj.gif"} alt="i made this"/>
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import {name, city} from  "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  
+   <div id="Home">
+    <h1 style={{color:"firebrick"}}>
+      {`Daniel`}Liza is a web developer from {`Cuba`}
+    </h1>
+  </div>
+  ) 
 }
 
 export default Home;
